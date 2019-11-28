4d238f0cc8f4bf4170f18a27adf7607a6514b79e
Elichai Turkel <elichai.turkel@gmail.com>
2019-11-28 13:30:50 +0200

    Use depends in s390x CI

--- ci/test/00_setup_env_s390x.sh
+++ ci/test/00_setup_env_s390x.sh
@@ -9,8 +9,7 @@ export LC_ALL=C.UTF-8
 # The host arch is unknown, so we run the tests through qemu.
 # If the host is s390x and wants to run the tests natively, it can set QEMU_USER_CMD to the empty string.
 export QEMU_USER_CMD="${QEMU_USER_CMD:"qemu-s390x"}"
-export PACKAGES="clang llvm python3-zmq qtbase5-dev qttools5-dev-tools libssl1.0-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
-export NO_DEPENDS=1
+export PACKAGES="python3 python3-zmq bsdmainutils qemu-user qtbase5-dev"
 export RUN_UNIT_TESTS=true
 export RUN_FUNCTIONAL_TESTS=true
 export GOAL="install"
